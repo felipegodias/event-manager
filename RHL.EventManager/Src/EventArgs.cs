e3da7b1437bf0d3e4a3bfebee2be81d3abf4607e
felipegodias <felipe.g.o.dias@outlook.com>
2017-07-22 03:40:15 -0300

    Added documentation to the src.

--- RHL.EventManager/Src/EventArgs.cs
+++ RHL.EventManager/Src/EventArgs.cs
@@ -1,5 +1,9 @@
 ﻿namespace RHL.EventManager {
 
+    /// <summary>
+    ///     Represents the base class for classes that contain event data, and provides a value to use for events that do not
+    ///     include event data.
+    /// </summary>
     public abstract class EventArgs {
 
     }
--- RHL.EventManager/Src/EventHandler.cs
+++ RHL.EventManager/Src/EventHandler.cs
@@ -1,5 +1,11 @@
 ﻿namespace RHL.EventManager {
 
+    /// <summary>
+    ///     Represents the method that will handle an event.
+    /// </summary>
+    /// <typeparam name="T">The type of the event data.</typeparam>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="eventArgs">An object that contains the event data.</param>
     public delegate void EventHandler<in T>(object sender, T eventArgs) where T : EventArgs;
 
 }
